f26d633949a6761a930d5f9ecc8f5d8b2e6fd752
dohun0310 <dohun0310@naver.com>
2024-02-21 17:07:40 +0900

    edit formattedData function

--- src/index.ts
+++ src/index.ts
@@ -23,10 +23,7 @@ const formattedDate = (date: Date) => {
   const month = date.getMonth() + 1;
   const day = date.getDate();
 
-  const formattedMonth = month < 10 ? `0${month}` : month;
-  const formattedDay = day < 10 ? `0${day}` : day;
-
-  return `${year}${formattedMonth}${formattedDay}`;
+  return `${year}-${month < 10 ? `0${month}` : month}-${day < 10 ? `0${day}` : day}`;
 };
 
 const getMonday = (date: Date) => {
