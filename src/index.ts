737bf63c7773ea305713659b3f30fa4686f687c8
dohun0310 <dohun0310@naver.com>
2024-02-22 11:11:06 +0900

    edit port

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express()
-const port = 3000
+const port = 8080
 
 const API_KEY = process.env.API_KEY;
 const BASE_URL = "https://open.neis.go.kr/hub/";
