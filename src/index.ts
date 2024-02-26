ed463c8c7b368e8e4f22fa47939be15b7fbfa282
dohun0310 <dohun0310@naver.com>
2024-02-27 02:17:04 +0900

    edit for production ready

--- src/index.ts
+++ src/index.ts
@@ -157,23 +157,24 @@ const fetchMeal = async (res: Response, startDate: string, endDate: string) => {
     if (response.data.mealServiceDietInfo && response.data.mealServiceDietInfo[1] && response.data.mealServiceDietInfo[1].row) {
       formatResponse(res, response.data.mealServiceDietInfo[1], "MLSV_YMD", "dish", "DDISH_NM", "calorie", "CAL_INFO");
     } else {
-      try {
-        /// TODO: NIES API에 식단표가 없을 경우, 학교 홈페이지 크롤링으로 식단표 가져오기(API 찾음)
-        // https://buyong-h.goeujb.kr/buyong-h/ad/fm/foodmenu/selectFoodData.do?fmSeq=16803 주간 API 날이 지날수록 fmSeq의 값이 +1 됨
+      notFoundResponse(res);
+      // try {
+      /// TODO: NIES API에 식단표가 없을 경우, 학교 홈페이지 크롤링으로 식단표 가져오기(API 찾음)
+      // https://buyong-h.goeujb.kr/buyong-h/ad/fm/foodmenu/selectFoodData.do?fmSeq=16803 주간 API 날이 지날수록 fmSeq의 값이 +1 됨
 
-        const url = "https://buyong-h.goeujb.kr/buyong-h/widgApi/get/json.do";
-        const response = await axios.get(url, {
-          params: {
-            widgSysId: "buyong-h",
-            widgId: "70"
-          }
-        });
+      //   const url = "https://buyong-h.goeujb.kr/buyong-h/widgApi/get/json.do";
+      //   const response = await axios.get(url, {
+      //     params: {
+      //       widgSysId: "buyong-h",
+      //       widgId: "70"
+      //     }
+      //   });
         
-        res.json(response.data);
-      } catch (error) {
-        console.log("ERROR: 학교 홈페이지 크롤링 실패", error);
-        notFoundResponse(res);
-      }
+      //   res.json(response.data);
+      // } catch (error) {
+      //   console.log("ERROR: 학교 홈페이지 크롤링 실패", error);
+      //   notFoundResponse(res);
+      // }
     }
   } catch (error) {
     errorResponse(res, error);
