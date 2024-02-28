a5d8cd80bec393a5f153095c7d009694557933e5
dohun0310 <dohun0310@naver.com>
2024-02-29 04:34:37 +0900

    edit config function

--- src/config.ts
+++ src/config.ts
@@ -6,3 +6,8 @@ export const API_KEY = process.env.API_KEY;
 export const BASE_URL = "https://open.neis.go.kr/hub/";
 export const OFFICE_CODE = "J10";
 export const SCHOOL_CODE = "7530575";
+
+if (!API_KEY) {
+  console.error("ERROR: .env 파일에 API_KEY가 존재하지 않기 때문에 종료합니다. 프로젝트 폴더 안에. env 파일이 있는지 확인하고, API_KEY=NEIS_API_KEY 형식으로 작성해야 합니다.");
+  process.exit(1);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,11 @@
 import express, { Request, Response } from "express";
 
-import { API_KEY } from "./config";
 import { today, monthstart, monthend, weekstart, weekend } from "./utils/date";
 import { fetchTimetable } from "./services/timetable";
 import { fetchMeal } from "./services/meal";
 
 const app = express()
 const port = 8080
-
-if (!API_KEY) {
-  console.error("ERROR: .env 파일에 API_KEY가 존재하지 않기 때문에 종료합니다. 프로젝트 폴더 안에. env 파일이 있는지 확인하고, API_KEY=NEIS_API_KEY 형식으로 작성해야 합니다.");
-  process.exit(1);
-}
 
 app.get("/", (req: Request, res: Response) => {
   res.redirect("https://github.com/dohun0310/BYHS-API");
