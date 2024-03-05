dfb0b13f9c92a75c9166866519444915d98437fe
dohun0310 <dohun0310@naver.com>
2024-03-06 00:33:23 +0900

    edit date function

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,51 +1,22 @@
-const formatDate = (date: Date): string => {
-  const year = date.getFullYear();
-  const month = (`0${date.getMonth() + 1}`).slice(-2);
-  const day = (`0${date.getDate()}`).slice(-2);
-  return `${year}${month}${day}`;
+export const formatDate = (date: Date = new Date()): string => {
+  return date.toISOString().slice(0, 10).replace(/-/g, '');
 };
 
-const getToday = (date: Date = new Date()): string => { return formatDate(date); };
+export const getToday = (): string => formatDate();
 
-const getMonthRange = (date: Date): { monthstart: string, monthend: string } => {
-  const startDay = new Date(date);
-  const currentMonth = startDay.getMonth();
-  const currentYear = startDay.getFullYear();
-  let endDay;
-
-  const lastDayOfMonth = new Date(currentYear, currentMonth + 1, 0);
-
-  if (startDay.getDate() > lastDayOfMonth.getDate() - 7) {
-    const nextMonth = currentMonth === 11 ? 0 : currentMonth + 1;
-    const nextYear = currentMonth === 11 ? currentYear + 1 : currentYear;
-    endDay = new Date(nextYear, nextMonth + 1, 0);
-  } else {
-    endDay = lastDayOfMonth;
-  }
-
+export const getMonthRange = (): { monthstart: string, monthend: string } => {
+  const date = new Date();
+  const startDay = new Date(date.getFullYear(), date.getMonth(), 1);
+  const endDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
   return { monthstart: formatDate(startDay), monthend: formatDate(endDay) };
 };
 
-const getWeekRange = (date: Date): { weekstart: string, weekend: string } => {
-  const today = new Date(date);
-  const dayOfWeek = today.getDay();
-  let endDay;
-
-  if (dayOfWeek >= 4) {
-    endDay = new Date(today);
-    endDay.setDate(today.getDate() + (12 - dayOfWeek));
-  } else {
-    endDay = new Date(today);
-    endDay.setDate(today.getDate() + (5 - dayOfWeek));
-  }
-
-  return { weekstart: formatDate(today), weekend: formatDate(endDay) };
+export const getWeekRange = (): { weekstart: string, weekend: string } => {
+  const today = new Date();
+  const firstDayOfWeek = new Date(today.setDate(today.getDate() - today.getDay() + (today.getDay() === 0 ? -6 : 1))); // Monday as the first day of the week
+  const lastDayOfWeek = new Date(firstDayOfWeek);
+  lastDayOfWeek.setDate(firstDayOfWeek.getDate() + 6); // Sunday as the last day
+  return { weekstart: formatDate(firstDayOfWeek), weekend: formatDate(lastDayOfWeek) };
 };
 
-export const today = getToday(new Date());
-
-export const { monthstart, monthend } = getMonthRange(new Date());
-
-export const { weekstart, weekend } = getWeekRange(new Date());
-
-export const dateFormatter = new Intl.DateTimeFormat("ko-KR", { month: "long", day: "numeric", weekday: "long" });
+export const dateFormatter = new Intl.DateTimeFormat("ko-KR", { year: 'numeric', month: "long", day: "numeric", weekday: "long" });
