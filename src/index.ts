6d3d1e4a437b3eb494a960beb8e453db50bf7ab6
dohun0310 <dohun0310@naver.com>
2024-03-06 00:33:56 +0900

    edit index.ts for date function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from "express";
 
-import { today, monthstart, monthend, weekstart, weekend } from "./utils/date";
+import { getToday, getMonthRange, getWeekRange } from "./utils/date";
 import { fetchTimetable } from "./services/timetable";
 import { fetchMeal } from "./services/meal";
 
@@ -13,21 +13,27 @@ app.get("/", (req: Request, res: Response) => {
 
 app.get("/getTodayTimeTable/:grade/:class", async (req: Request, res: Response) => {
   const { grade, class: classNumber } = req.params;
+  const today = getToday();
 
-  fetchTimetable(res, grade, classNumber, today, today);
+  fetchTimetable(res, Number(grade), Number(classNumber), today, today);
 })
 
 app.get("/getWeekTimeTable/:grade/:class", async (req: Request, res: Response) => {
   const { grade, class: classNumber } = req.params;
+  const { weekstart, weekend } = getWeekRange();
 
-  fetchTimetable(res, grade, classNumber, weekstart, weekend);
+  fetchTimetable(res, Number(grade), Number(classNumber), weekstart, weekend);
 })
 
 app.get("/getTodayMeal", async (req: Request, res: Response) => {
+  const today = getToday();
+
   fetchMeal(res, today, today);
 })
 
 app.get("/getMonthMeal", async (req: Request, res: Response) => {
+  const { monthstart, monthend } = getMonthRange();
+
   fetchMeal(res, monthstart, monthend);
 })
 
