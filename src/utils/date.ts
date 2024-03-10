647c65983b9e5ffb574084ad9ec7e470c6cf10cf
dohun0310 <dohun0310@naver.com>
2024-03-11 01:39:53 +0900

    edit to apply KST in date function

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,21 +1,22 @@
 export const formatDate = (date: Date = new Date()): string => {
-  return date.toISOString().slice(0, 10).replace(/-/g, '');
+  const kst = 9 * 60 * 60 * 1000;
+  const localDate = new Date(date.getTime() + kst);
+  return localDate.toISOString().slice(0, 10).replace(/-/g, '');
 };
 
 export const getToday = (): string => formatDate();
 
 export const getMonthRange = (): { monthstart: string, monthend: string } => {
   const date = new Date();
-  const startDay = new Date(date.getFullYear(), date.getMonth(), 1);
-  const endDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+  const startDay = new Date(Date.UTC(date.getFullYear(), date.getMonth(), 1));
+  const endDay = new Date(Date.UTC(date.getFullYear(), date.getMonth() + 1, 0));
   return { monthstart: formatDate(startDay), monthend: formatDate(endDay) };
 };
 
 export const getWeekRange = (): { weekstart: string, weekend: string } => {
   const today = new Date();
-  const firstDayOfWeek = new Date(today.setDate(today.getDate() - today.getDay() + (today.getDay() === 0 ? -6 : 1)));
-  const lastDayOfWeek = new Date(firstDayOfWeek);
-  lastDayOfWeek.setDate(firstDayOfWeek.getDate() + 6);
+  const firstDayOfWeek = new Date(today.getTime() - (today.getDay() * 24 * 60 * 60 * 1000) + ((today.getDay() === 0 ? -6 : 1) * 24 * 60 * 60 * 1000));
+  const lastDayOfWeek = new Date(firstDayOfWeek.getTime() + (6 * 24 * 60 * 60 * 1000));
   return { weekstart: formatDate(firstDayOfWeek), weekend: formatDate(lastDayOfWeek) };
 };
 
